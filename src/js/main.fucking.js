5ac04c9aeef222def43b4f6917faa10d8967793b
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-12-05 16:36:14 +0430

    added info

--- src/js/main.fucking.js
+++ src/js/main.fucking.js
@@ -1,6 +1,26 @@
 "use strict";
+// Latest blog posts
+const blogPostsContainer = document.querySelector('.blog_posts');
+const response = await fetch(
+  'https://blog.shahryartayeb.com/api/featured-posts'
+);
+const posts = await response.json();
+
+posts.forEach((post) => {
+  // insert into the page
+  const blogPostMarkup = `
+<li>
+    <a href="https://blog.shahryartayeb.com/post/${post.slug}">
+      ${post.title}
+    </a>
+</li>
+`;
+  blogPostsContainer.insertAdjacentHTML('afterbegin', blogPostMarkup);
+});
+
+
 // I wish I could have used ES6 extravaganza, but not everyone supports it :(
-var r = document.getElementById("rbw"),
+let r = document.getElementById("rbw"),
   currentHue = 0,
   hueAddition = 5,
   documentElement = document.getElementsByTagName("html")[0],
@@ -10,13 +30,13 @@ var r = document.getElementById("rbw"),
   stringArray = ["Add more contrast", "Remove additional contrast", "Inverted mode", "Normal mode"];
 
 function createControls() {
-  var contrastButton = document.createElement('button');
+  let contrastButton = document.createElement('button');
     contrastButton.id = "contrast";
     contrastButton.classList.add('cont-inv');
     contrastButton.innerText = stringArray[0];
     contrastButton.tabIndex = 1;
 
-  var nightModeButton = document.createElement('button');
+  let nightModeButton = document.createElement('button');
     nightModeButton.id = "invmode";
     nightModeButton.classList.add('cont-inv');
     nightModeButton.innerText = stringArray[2];
@@ -26,7 +46,7 @@ function createControls() {
 }
 
 function doThatFuckingColorThing() {
-  var color = "hsl(" + currentHue + ", 80%, 60%)",
+  let color = "hsl(" + currentHue + ", 80%, 60%)",
     nextHue = currentHue + hueAddition;
   currentHue = nextHue > 360 ? 0 : nextHue;
   r.style.color = color;
@@ -38,14 +58,14 @@ function someControl(id, textArr, className) {
    * http://www.vanilla-js.com/
    * http://jsperf.com/getelementbyid-vs-jquery-id/44
    */
-  var el = document.getElementsByTagName("html")[0];
-  var acbox = document.getElementById(id),
+  let el = document.getElementsByTagName("html")[0];
+  let acbox = document.getElementById(id),
     textNode = acbox.firstChild,
     toggled = false;
   acbox.addEventListener(
     clickEvent,
     function() {
-      var selector = Number((toggled = !toggled));
+      let selector = Number((toggled = !toggled));
       textNode.data = textArr[selector];
       el.classList[classMethods[selector]](className);
     },
