781650eccd0717e3b7344571763e8fefed6448c5
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-12-06 09:16:16 +0430

    WIP

--- src/js/main.fucking.js
+++ src/js/main.fucking.js
@@ -1,55 +1,45 @@
-"use strict";
+'use strict';
 // Latest blog posts
 const blogPostsContainer = document.querySelector('.blog_posts');
-const response = await fetch(
-  'https://blog.shahryartayeb.com/api/featured-posts'
-);
-const posts = await response.json();
 
-posts.forEach((post) => {
-  // insert into the page
-  const blogPostMarkup = `
-<li>
-    <a href="https://blog.shahryartayeb.com/post/${post.slug}">
-      ${post.title}
-    </a>
-</li>
-`;
-  blogPostsContainer.insertAdjacentHTML('afterbegin', blogPostMarkup);
-});
+
 
 
 // I wish I could have used ES6 extravaganza, but not everyone supports it :(
-let r = document.getElementById("rbw"),
+let rbw = document.getElementById('rbw'),
   currentHue = 0,
   hueAddition = 5,
-  documentElement = document.getElementsByTagName("html")[0],
-  clickEvent = "ontouchstart" in window ? "touchend" : "click",
-  classMethods = ["remove", "add"],
+  documentElement = document.getElementsByTagName('html')[0],
+  clickEvent = 'ontouchstart' in window ? 'touchend' : 'click',
+  classMethods = ['remove', 'add'],
   rainbowTiming = 1000 / 25,
-  stringArray = ["Add more contrast", "Remove additional contrast", "Inverted mode", "Normal mode"];
+  stringArray = [
+    'Add more contrast',
+    'Remove additional contrast',
+    'Inverted mode',
+    'Normal mode',
+  ];
 
 function createControls() {
   let contrastButton = document.createElement('button');
-    contrastButton.id = "contrast";
-    contrastButton.classList.add('cont-inv');
-    contrastButton.innerText = stringArray[0];
-    contrastButton.tabIndex = 1;
+  contrastButton.id = 'contrast';
+  contrastButton.classList.add('cont-inv');
+  contrastButton.innerText = stringArray[0];
+  contrastButton.tabIndex = 1;
 
   let nightModeButton = document.createElement('button');
-    nightModeButton.id = "invmode";
-    nightModeButton.classList.add('cont-inv');
-    nightModeButton.innerText = stringArray[2];
-    nightModeButton.tabIndex = 2;
+  nightModeButton.id = 'invmode';
+  nightModeButton.classList.add('cont-inv');
+  nightModeButton.innerText = stringArray[2];
+  nightModeButton.tabIndex = 2;
   document.body.appendChild(contrastButton);
   document.body.appendChild(nightModeButton);
 }
 
 function doThatFuckingColorThing() {
-  let color = "hsl(" + currentHue + ", 80%, 60%)",
+  let color = 'hsl(' + currentHue + ', 80%, 60%)',
     nextHue = currentHue + hueAddition;
   currentHue = nextHue > 360 ? 0 : nextHue;
-  r.style.color = color;
   setTimeout(doThatFuckingColorThing, rainbowTiming);
 }
 
@@ -58,13 +48,13 @@ function someControl(id, textArr, className) {
    * http://www.vanilla-js.com/
    * http://jsperf.com/getelementbyid-vs-jquery-id/44
    */
-  let el = document.getElementsByTagName("html")[0];
+  let el = document.getElementsByTagName('html')[0];
   let acbox = document.getElementById(id),
     textNode = acbox.firstChild,
     toggled = false;
   acbox.addEventListener(
     clickEvent,
-    function() {
+    function () {
       let selector = Number((toggled = !toggled));
       textNode.data = textArr[selector];
       el.classList[classMethods[selector]](className);
@@ -74,18 +64,36 @@ function someControl(id, textArr, className) {
 }
 
 function addContrastControl() {
-  someControl(
-    "contrast",
-    [stringArray[0], stringArray[1]],
-    "contrast"
-  );
+  someControl('contrast', [stringArray[0], stringArray[1]], 'contrast');
 }
 
 function addInvertedControl() {
-  someControl("invmode", [stringArray[2], stringArray[3]], "inverted");
+  someControl('invmode', [stringArray[2], stringArray[3]], 'inverted');
 }
 
 createControls();
 doThatFuckingColorThing();
 addContrastControl();
 addInvertedControl();
+
+
+try {
+  const response = await fetch(
+    'https://blog.shahryartayeb.com/api/featured-posts'
+  );
+  const posts = await response.json();
+
+  posts.forEach((post) => {
+    // insert into the page
+    const blogPostMarkup = `
+<li>
+    <a href="https://blog.shahryartayeb.com/post/${post.slug}">
+      ${post.title}
+    </a>
+</li>
+`;
+    blogPostsContainer.insertAdjacentHTML('afterbegin', blogPostMarkup);
+  });
+} catch (error) {
+  console.log(error);
+}
